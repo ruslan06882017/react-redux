5c48d66174506abe6ae378f00e60beb83f263bec
Ruslan Gusseinov <r.gusseinov@mail.ru>
2020-04-07 00:17:44 +0600

    Redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux'
+import { increment, decrement, reset} from './actions';
 
-function App() {
+function App({ counter, increment, decrement, reset}) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={increment}>Plus</button>
+      <button onClick={decrement}>Minus</button>
+      <button onClick={reset}>Reset</button>
+      <h1>{counter}</h1>
     </div>
   );
 }
 
-export default App;
+export default connect(state => ({
+  counter: state.counter
+}), { increment, decrement, reset })(App);
