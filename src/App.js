821d95e0d7645615a91247499aabcfa6596462ce
Ruslan Gusseinov <r.gusseinov@mail.ru>
2020-04-07 19:14:49 +0600

    prepared for redux test work

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React from 'react';
-import { connect } from 'react-redux'
-import { increment, decrement, reset} from './actions';
 
-function App({ counter, increment, decrement, reset}) {
+function App() {
   return (
     <div className="App">
-      <button onClick={increment}>Plus</button>
-      <button onClick={decrement}>Minus</button>
-      <button onClick={reset}>Reset</button>
-      <h1>{counter}</h1>
+        Hello Bitches
     </div>
   );
 }
 
-export default connect(state => ({
-  counter: state.counter
-}), { increment, decrement, reset })(App);
+export default App;
